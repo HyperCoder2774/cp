0bf5943b1e81e9ce058a8c8d6b72a92d78674b81
HyperCoder2774 <75514076+HyperCoder2774@users.noreply.github.com>
2021-03-18 09:57:37 +0530

    Update Statement for Code.cpp

--- Statement for Code.cpp
+++ Statement for Code.cpp
@@ -1,1 +1,3 @@
-By real time of Arrival & Dipar. of a train and The time train is late in way from i to i+1 th station we can find out Arrival time of the train in the last station or any i(>0)th station
+By real time of Arrival & Dipar. of a train and The time train is 
+late in way from i to i+1 th station we can find out Arrival time 
+of the train in the last station or any i(>0)th station
